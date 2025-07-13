a8bd1dca4b0dcc570d101c7d6e96edecf460f9f8
ThatGuySid <143327553+ThatGuySid@users.noreply.github.com>
2025-07-13 17:20:21 +0530

    Update message.jsx

--- hrms_chat/src/components/message.jsx
+++ hrms_chat/src/components/message.jsx
@@ -1,12 +1,18 @@
-import notification from '../assets/notification.png';
-function Message(){
+import React from 'react';
+
+const Message = ({ text, sender, specialReply }) => {
+    const lines = text.split('\n');
     return (
-        <div className="message">
-            <h2>Messages
-            <img alt="message" src={notification} className='notify'/>
-            </h2>
-            
+        <div className={`message ${sender === 'me' ? 'sent' : 'received'}`}>
+            <div className={`bubble ${sender === 'me' ? 'gray' : 'purple'}`}>
+                <div>{lines[0]}</div>
+                {specialReply && (
+                    <div className="reply-options">Not yet &nbsp;&nbsp; Yes</div>
+                )}
+                {!specialReply && lines[1] && <div className="timestamp">{lines[1]}</div>}
+            </div>
         </div>
-    )
-}
-export default Message
+    );
+};
+
+export default Message;
